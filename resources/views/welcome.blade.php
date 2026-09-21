927c77051cdef1ae15bfacd17d557483ce956a34
I Dewa Gede Cahya Adi Utama <cahya.adi92@gmail.com>
2026-09-21 08:00:11 +0800

    Update title landing in tab

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ config('app.name', 'WDD') }} - Landing</title>
+    <title>Paiketan Warga Dura Desa - Landing</title>
     
     <!-- Google Fonts: Inter -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
