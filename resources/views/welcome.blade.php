ca576be8b349c0f892c6abb8496ea9fc1ec3a6da
I Dewa Gede Cahya Adi Utama <cahya.adi92@gmail.com>
2026-09-21 08:03:35 +0800

    Add WDD logo to the tab

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Paiketan Warga Dura Desa - Landing</title>
-    
+    <link rel="icon" type="image/png" href="{{ asset('images/Logo-WDD.png') }}">
     <!-- Google Fonts: Inter -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
